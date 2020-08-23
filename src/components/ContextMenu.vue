63f7f8e3f0a0ea506852105064f959a00baa843e
Ariel <arielqhou@gmail.com>
2020-08-23 19:04:19 -0400

    Add in untracked files.

---
+++ src/components/ContextMenu.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="grid">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class ContextMenu extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+@import "@/assets/styles/components/context-menu.scss";
+</style>
---
+++ src/components/old/ContentBox.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="box" :class="{ small: isNarrow }">
+    <h1 v-if="title" :class="{ header: hasHeader }">{{ title }}</h1>
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class ContentBox extends Vue {
+  @Prop() private title?: string;
+  @Prop() private isNarrow!: boolean;
+  @Prop() private hasHeader?: boolean;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+@import "@/assets/styles/components/content-box.scss";
+</style>
